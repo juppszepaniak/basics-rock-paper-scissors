1ad489e15656c7ff981f7403a93573f904ec3892
Jupp Szepaniak <sze.jupp2001@gmail.com>
2024-11-30 20:03:39 +0100

    trial and error
    - changing color with js
    - styling

--- script.js
+++ script.js
@@ -20,41 +20,55 @@ function selectChoice() {
     you = this.id; /* refers to the id of choice*/
     document.getElementById("your-choice").src = "assets/" + you + ".png"
 
-    //random fpr opponent
+    //random for opponent
     opponent = choices[Math.floor(Math.random() * 3)]; //floor --> makes 1.999 to 1 --> 0, 1 , 2 (Array)
     document.getElementById("opponent-choice").src = "assets/" + opponent + ".png";
 
-    //check for winner --> Add Green to Winner
+    document.getElementById("your-score").style.color = "rgb(78, 78, 78)";
+    document.getElementById("opponent-score").style.color = "rgb(78, 78, 78)";
+
+    //check for winner
     if (you == opponent) {
-        yourScore += 1;
-        opponentScore += 1;
+        yourScore += 0;
+        document.getElementById("your-score").style.color = "rgb(53, 102, 175)";
+        opponentScore += 0;
+        document.getElementById("opponent-score").style.color = "rgb(53, 102, 175)";
     } else {
         if (you == "rock") {
             if (opponent == "scissors") {
                 yourScore += 1;
+                document.getElementById("your-score").style.color = "rgb(60, 143, 88)";
+                document.getElementById("opponent-score").style.color = "rgb(143, 60, 60)";
             } else if (opponent == "paper"){
                 opponentScore += 1;
+                document.getElementById("opponent-score").style.color = "rgb(60, 143, 88)";
+                document.getElementById("your-score").style.color = "rgb(143, 60, 60)";
             }
         }
-        if (you == "paper") {
+        else if (you == "paper") {
             if (opponent == "rock") {
                 yourScore += 1;
-            } else if (opponent == "cissors"){
+                document.getElementById("your-score").style.color = "rgb(60, 143, 88)";
+                document.getElementById("opponent-score").style.color = "rgb(143, 60, 60)";
+            } else if (opponent == "scissors"){
                 opponentScore += 1;
+                document.getElementById("opponent-score").style.color = "rgb(60, 143, 88)";
+                document.getElementById("your-score").style.color = "rgb(143, 60, 60)";
             }
         }
-        if (you == "scissors") {
+        else if (you == "scissors") {
             if (opponent == "paper") {
                 yourScore += 1;
+                document.getElementById("your-score").style.color = "rgb(60, 143, 88)";
+                document.getElementById("opponent-score").style.color = "rgb(143, 60, 60)";
             } else if (opponent == "rock"){
                 opponentScore += 1;
+                document.getElementById("opponent-score").style.color = "rgb(60, 143, 88)";
+                document.getElementById("your-score").style.color = "rgb(143, 60, 60)";
             }
         }
     }
 
-    document.getElementById("opponent-score").innerText = yourScore;
-    document.getElementById("your-score").innerText = opponentScore;
-}
-
-function resetGame() {
+    document.getElementById("opponent-score").innerText = opponentScore;
+    document.getElementById("your-score").innerText = yourScore;
 }
